0d1289ab100f6f58bd2264ac98336ffec2db6e21
Elham-EN <ilhamrezaie@gmail.com>
2024-04-16 23:02:16 +1000

    setup firebase admin SDK with Express app

---
+++ src/constant.ts
@@ -0,0 +1,2 @@
+export const serviceAccountKey = process.env.SERVICE_ACCOUNT_KEY as string;
+export const serverPort = (process.env.SERVER_PORT as string) || (7000 as number);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-console.log("====================================");
-console.log("Hello Node.js");
-console.log("====================================");
+import express from "express";
+import admin from "firebase-admin";
+import "dotenv/config";
+import { serverPort, serviceAccountKey } from "./constant";
+
+// Initialize the SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+const app = express();
+
+app.listen(serverPort, () => {
+  console.log(`Server listening on port ${serverPort}`);
+});
